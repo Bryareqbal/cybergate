8502c6244f42ac907d0ac9d6235502f7ebf09939
bryareqbal <briar.eqbal@gmail.com>
2022-07-03 14:17:03 +0300

    adding livewire and layouts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Dashboard;
+
+Route::get('/test', Dashboard::Class)->name('root');
 
 Route::get('/', function () {
     return view('welcome');

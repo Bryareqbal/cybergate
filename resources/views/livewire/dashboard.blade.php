e6861b2f5edfeb7fd9d5bcfa171e9428b8d4d268
Hoshmand Kamal <hoshmandG900@gmail.com>
2022-07-17 14:28:09 +0300

    dashboard page

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -111,7 +111,7 @@
                     <div class="flex flex-row justify-between group-hover:text-gray-200 ">
                         <p class="text-sm mt-1 text-gray-500">Current Data</p>
 
-                        <p class="text-sm mt-1 text-gray-500">12</p>
+                        <p class="text-sm mt-1 text-gray-500">{{ $find_data_asayish }}</p>
 
                     </div>
                 </div>
